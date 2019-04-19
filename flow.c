93d54191cf9a003867f4811026310afebe9e0735
ksmountakis <k.s.mountakis@gmail.com>
2019-04-20 00:41:02 +0300

    Refactoring.

--- flow.c
+++ flow.c
@@ -17,7 +17,27 @@ typedef struct {
 	float s;
 } chain_node_t;
 
-int m, n, N;
+typedef struct {
+	int (*prioritize)(const void*, const void*);
+	int *a, *o;
+	float *s;
+	int *v;
+	float *tardiness, *best_objective_tardiness;
+	int *inserted;
+	int ***arc;
+	tuple_t *tuples;
+	chain_node_t **chain;
+	float objective, max_tard;
+} work_t;
+
+typedef struct {
+	int N, n, m;
+	float T, Q, L;
+	int i, j, k, z;
+	float *x, *y, *d, *q, *e, *l;
+	double **t;
+} darp_t;
+
 
 int priority_criterion(const void *t1ptr, const void *t2ptr) {
 	tuple_t *t1 = (tuple_t*)t1ptr;
@@ -39,164 +59,116 @@ int priority_criterion(const void *t1ptr, const void *t2ptr) {
 	return +1;
 }
 
-int
-main(void)
+static void
+work_create(work_t *work, darp_t darp)
 {
-	int err;
-	float T, Q, L;
-	int i, j, k, z;
-	float *x, *y, *d, *q, *e, *l;
-	double **t;
-	int *a, *o;
+	int i, j, k;
 
-	int num_inserted;
-	float *s;
-	int *v;
-	float *tardiness, *best_objective_tardiness;
-	int *inserted;
-	int ***arc;
-	int p;
-	float tard, best_tard;
-	int best_i, best_k, best_found;
-	float objective;
-	float max_tard;
-	int kmin, kmax;
-	tuple_t *tuples;
-	chain_node_t **chain;
-	int num_restarts, max_restarts = 1000;
-	float best_objective = RAND_MAX;
+	work->prioritize = priority_criterion;
 
-	// <m> <n> <max veh ride minutes Tk> <veh capacity Qk> <max user ride minutes Li>
-	err = scanf("%d %d %f %f %f\n", &m, &N, &T, &Q, &L);
-	if (err != 5) {
-		fprintf(stderr, "error processing line 1\n");
-		exit(EXIT_FAILURE);
+	assert((work->tuples = calloc(sizeof(tuple_t), darp.N)));
+	assert((work->tardiness = calloc(sizeof(float), darp.N)));
+	assert((work->s = calloc(sizeof(float), darp.N)));
+	assert((work->v = calloc(sizeof(int), darp.n)));
+	assert((work->best_objective_tardiness = calloc(sizeof(float), darp.N)));
+	assert((work->inserted = calloc(sizeof(int), darp.N)));
+	assert((work->arc = calloc(sizeof(int*), darp.N)));
+	for (i=0; i < darp.N; i++) {
+		assert((work->arc[i] = calloc(sizeof(int*), darp.N)));
+		for (j=0; j < darp.N; j++)
+			assert((work->arc[i][j] = calloc(sizeof(int), darp.m)));
 	}
+	assert((work->a = calloc(sizeof(int), darp.N)));
+	assert((work->o = calloc(sizeof(int), darp.N)));
+	assert((work->chain = calloc(sizeof(chain_node_t*), darp.m)));
+	for (k=0; k < darp.m; k++) 
+		assert((work->chain[k] = calloc(sizeof(chain_node_t), darp.N)));
 
-	N = 2*N + 2;
-	n = (N-2)/2;
+}
 
-	fprintf(stderr, "n %d m %d\n", n, m);
-
-	// allocations
-	assert((x = calloc(sizeof(float), N)));
-	assert((y = calloc(sizeof(float), N)));
-	assert((d = calloc(sizeof(float), N)));
-	assert((q = calloc(sizeof(float), N)));
-	assert((e = calloc(sizeof(float), N)));
-	assert((l = calloc(sizeof(float), N)));
-	assert((t = calloc(sizeof(double*), N)));
-	for (i=0; i < N; i++)
-		assert((t[i] = calloc(sizeof(double), N)));
-
-	assert((s = calloc(sizeof(float), N)));
-	assert((v = calloc(sizeof(int), n)));
-	assert((tardiness = calloc(sizeof(float), N)));
-	assert((best_objective_tardiness = calloc(sizeof(float), N)));
-	assert((inserted = calloc(sizeof(int), N)));
-	assert((arc = calloc(sizeof(int*), N)));
-	for (i=0; i < N; i++) {
-		assert((arc[i] = calloc(sizeof(int*), N)));
-		for (j=0; j < N; j++)
-			assert((arc[i][j] = calloc(sizeof(int), m)));
-	}
-	assert((tuples = calloc(sizeof(tuple_t), N)));
-	assert((a = calloc(sizeof(int), N)));
-	assert((o = calloc(sizeof(int), N)));
-	assert((chain = calloc(sizeof(chain_node_t*), m)));
-	for (k=0; k < m; k++) 
-		assert((chain[k] = calloc(sizeof(chain_node_t), N)));
-	
-	for (i=0; i < N; i++) {
-		int id;
-		// <i> <x coord xi> <y coord yi> <service time at node di> <num passengers qi> <earliest time ei> <latest time li>
-		err = scanf("%d %f %f %f %f %f %f\n", &id, &x[i], &y[i], &d[i], &q[i], &e[i], &l[i]);
-		if (err != 7) {
-			fprintf(stderr, "error processing line %d\n", i+2);
-			exit(EXIT_FAILURE);
-		}
-
-		for (j=0; j <= i; j++)
-			t[i][j] = sqrt(pow(x[j]-x[i],2) + pow(y[j]-y[i],2));
-	}
-
-	num_restarts = 0;
-restart:
+static void
+work_prioritize(work_t *work, darp_t darp)
+{
+	int i, z;
 	// sort according to priority criterion
-	for (i=0; i < N; i++) {
-		tuples[i].i = i;
-		tuples[i].e = e[i];
-		//tuples[i].l = l[i] - (0.5*rand()/RAND_MAX*tardiness[i]);
-		tuples[i].l = l[i] - 0.5*tardiness[i]*rand()/RAND_MAX;
-		tuples[i].source = (i == 0);
-		tuples[i].sink = (i == N-1);
+	for (i=0; i < darp.N; i++) {
+		work->tuples[i].i = i;
+		work->tuples[i].e = darp.e[i];
+		work->tuples[i].l = darp.l[i] - 0.5*work->tardiness[i]*rand()/RAND_MAX;
+		work->tuples[i].source = (i == 0);
+		work->tuples[i].sink = (i == darp.N-1);
 	}
-	qsort(tuples, N, sizeof(tuple_t), priority_criterion);
+	qsort(work->tuples, darp.N, sizeof(tuple_t), work->prioritize);
 
 	// initialize priority rule
-	//printf("a: ");
-	for (z=0; z < N; z++) {
-		a[z] = tuples[z].i;
-		o[tuples[z].i] = z;
-		//printf("%2d ", a[z]);
+	for (z=0; z < darp.N; z++) {
+		work->a[z] = work->tuples[z].i;
+		work->o[work->tuples[z].i] = z;
 	}
-	//puts("");
+}
+
+static void
+work_schedule(work_t *work, darp_t darp)
+{
+	int i, j, k, p, kmin, kmax;
+	int num_inserted;
+	float tard, best_tard;
+	int best_k, best_found;
 
 	// initialize chain-form schedule
-	for (k=0; k < m; k++) {
-		chain[k][0].i = 0;
-		chain[k][0].leak = Q;
+	for (k=0; k < darp.m; k++) {
+		work->chain[k][0].i = 0;
+		work->chain[k][0].leak = darp.Q;
 	}
 
 	// generate schedule
-	s[0] = 0;
+	work->s[0] = 0;
 	num_inserted = 0;
-	memset(inserted, 0, sizeof(int)*N);
-	memset(tardiness, 0, sizeof(float)*N);
-	objective = 0;
-	max_tard = 0;
+	memset(work->inserted, 0, sizeof(int)*darp.N);
+	memset(work->tardiness, 0, sizeof(float)*darp.N);
 
-	while (num_inserted < N-2)
+	work->objective = 0;
+	work->max_tard = 0;
+	while (num_inserted < darp.N-2)
 	{
 		// pick next eligible event from priority rule a[]
-		for (p=0; p < N; p++) {
-			j = a[p];
+		for (p=0; p < darp.N; p++) {
+			j = work->a[p];
 
-			if (j == 0 || j == N-1) 
+			if (j == 0 || j == darp.N-1) 
 				continue;
 
 			//printf("\n%d ", j);
 
-			if (inserted[j]) {
+			if (work->inserted[j]) {
 				//printf("already inserted");
 				continue;
 			}
 
-			if (j > n && j < 2*n+1) {
-				if (!inserted[j-n]) {
+			if (j > darp.n && j < 2*darp.n+1) {
+				if (!work->inserted[j-darp.n]) {
 					//printf("pickup not yet inserted");
 					continue;
 				}
-				kmin = kmax = v[j-n];
+				kmin = kmax = work->v[j-darp.n];
 			} else {
 				kmin = 0;
-				kmax = m-1;
+				kmax = darp.m-1;
 			}
 
-			best_tard = 10*T;
-			best_i = best_found = best_k = 0;
+			best_tard = 10*darp.T;
+			best_found = best_k = 0;
 
 			for (k=kmin; k <= kmax; k++) {
-				if (chain[k][0].leak < q[j]) 
+				if (work->chain[k][0].leak < darp.q[j]) 
 					continue;
 
-				i = chain[k][0].i;
-				s[j] = s[i] + t[i][j] + d[i];
-				s[j] = fmax(e[j], s[j]);
-				tard = s[j] - l[j];
+				i = work->chain[k][0].i;
+				work->s[j] = work->s[i] + darp.t[i][j] + darp.d[i];
+				work->s[j] = fmax(darp.e[j], work->s[j]);
+				tard = work->s[j] - darp.l[j];
 
 				if (tard <= best_tard) {
-					best_i = i;
 					best_k = k;
 					best_tard = tard;
 					best_found = 1;
@@ -204,16 +176,16 @@ restart:
 			}
 
 			if (best_found) {
-				objective += t[chain[best_k][0].i][j];
-				max_tard = fmax(max_tard, best_tard);
+				work->objective += darp.t[work->chain[best_k][0].i][j];
+				work->max_tard = fmax(work->max_tard, best_tard);
 				//printf("(%d,%d):%d\n", chain[best_k][0].i, j, k);
-				arc[chain[best_k][0].i][j][best_k] = 1;
-				chain[best_k][0].i = j;
-				chain[best_k][0].leak -= q[j];
-				inserted[j] = 1;
-				tardiness[j] = best_tard;
-				if (j <= n)
-					v[j] = best_k;
+				work->arc[work->chain[best_k][0].i][j][best_k] = 1;
+				work->chain[best_k][0].i = j;
+				work->chain[best_k][0].leak -= darp.q[j];
+				work->inserted[j] = 1;
+				work->tardiness[j] = best_tard;
+				if (j <= darp.n)
+					work->v[j] = best_k;
 				num_inserted++;
 				//printf("inserted %d (k=%d leak=%d tard=%.1f)\n", j, best_k, chain[best_k][0].leak, best_tard);
 			} else {
@@ -223,49 +195,81 @@ restart:
 	}
 
 	// append sink to all machines
-	for (k=0; k < m; k++) {
-		i = chain[k][0].i;
-		arc[i][N-1][k] = 1;
-		s[N-1] = fmax(s[N-1], s[i]+d[i]+t[i][N-1]);
+	for (k=0; k < darp.m; k++) {
+		i = work->chain[k][0].i;
+		work->arc[i][darp.N-1][k] = 1;
+		work->s[darp.N-1] = fmax(work->s[darp.N-1], work->s[i] + darp.d[i] + darp.t[i][darp.N-1]);
 	}
 
-#if 1
-	// verify constraints
-	for (i=0; i < N; i++) {
-		int sum = 0;
-		for (k=0; k < m; k++) {
-			for (j=0; j < N; j++) {
-				sum += arc[i][j][k];
-			}
+}
+
+static void
+darp_create(darp_t *darp)
+{
+	int i, j, err;
+	// <m> <n> <max veh ride minutes Tk> <veh capacity Qk> <max user ride minutes Li>
+	err = scanf("%d %d %f %f %f\n", &darp->m, &darp->N, &darp->T, &darp->Q, &darp->L);
+	if (err != 5) {
+		fprintf(stderr, "error processing line 1\n");
+		exit(EXIT_FAILURE);
+	}
+
+	darp->N = 2*darp->N + 2;
+	darp->n = (darp->N-2)/2;
+
+	assert((darp->x = calloc(sizeof(float), darp->N)));
+	assert((darp->y = calloc(sizeof(float), darp->N)));
+	assert((darp->d = calloc(sizeof(float), darp->N)));
+	assert((darp->q = calloc(sizeof(float), darp->N)));
+	assert((darp->e = calloc(sizeof(float), darp->N)));
+	assert((darp->l = calloc(sizeof(float), darp->N)));
+	assert((darp->t = calloc(sizeof(double*), darp->N)));
+	for (i=0; i < darp->N; i++)
+		assert((darp->t[i] = calloc(sizeof(double), darp->N)));
+
+	for (i=0; i < darp->N; i++) {
+		int id;
+		// <i> <x coord xi> <y coord yi> <service time at node di> <num passengers qi> <earliest time ei> <latest time li>
+		err = scanf("%d %f %f %f %f %f %f\n", &id, &darp->x[i], &darp->y[i], &darp->d[i], &darp->q[i], &darp->e[i], &darp->l[i]);
+		if (err != 7) {
+			fprintf(stderr, "error processing line %d\n", i+2);
+			exit(EXIT_FAILURE);
 		}
-		//printf("sum {j in N, k in K} arc[%d,j,k] (%d)\n", i, sum);
+
+		for (j=0; j <= i; j++)
+			darp->t[i][j] = sqrt(pow(darp->x[j]-darp->x[i],2) + pow(darp->y[j]-darp->y[i],2));
 	}
-#endif
+}
 
-	if (max_tard < best_objective) {
-		printf("objective %f max_tard %f\n", objective, max_tard);
-		best_objective = max_tard;
-		memcpy(best_objective_tardiness, tardiness, sizeof(float)*N);
-	} else {
-		memcpy(tardiness, best_objective_tardiness, sizeof(float)*N);
+
+int
+main(void)
+{
+	int restart, maxrestarts = 200;
+	float best_objective = RAND_MAX;
+	float *best_objective_tardiness;
+
+	darp_t darp;
+	work_t work;
+
+	darp_create(&darp);
+	work_create(&work, darp);
+
+	assert((best_objective_tardiness = calloc(sizeof(float), darp.N)));
+
+	for (restart=0; restart < maxrestarts; restart++) {
+
+		work_prioritize(&work, darp);
+		work_schedule(&work, darp);
+
+		if (work.max_tard < best_objective) {
+			printf("objective %f max_tard %f\n", work.objective, work.max_tard);
+			best_objective = work.max_tard;
+			memcpy(best_objective_tardiness, work.tardiness, sizeof(float)*darp.N);
+		} else {
+			memcpy(work.tardiness, best_objective_tardiness, sizeof(float)*darp.N);
+		}
 	}
 
-	if (num_restarts++ < max_restarts)
-		goto restart;
-
-#if 0
-	// process each node in order
-	printf("P: ");
-	for (i=1; i <= n; i++) 
-		printf("%2d:%2d ", i, o[i]);
-	puts("");
-
-	printf("D: ");
-	for (i=1+n; i <= 2*n; i++) 
-		printf("%2d:%2d ", i, o[i]);
-
-	printf("%2d:%2d\n", N-1, o[N-1]);
-#endif
-	
 	return 0;
 }
